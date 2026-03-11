42f96258bf47540875e5ee19256a3e36998de322
evillar@paterostechnologicalcollege.edu.ph <evillar@paterostechnologicalcollege.edu.ph>
2026-03-12 00:34:53 +0800

    Create grp2_villar_ex3.py

---
+++ grp2_villar_ex3.py
@@ -0,0 +1,27 @@
+print("Hello, I'm Era B. Villar! I'm from Bsit 1V")
+
+#my first simple script#python comments
+
+#python variables.
+
+ab 18
+cd 19
+
+print (ab)
+print(cd)
+
+#python variable names.
+
+lyka "Lyka" 
+angel "Angel"
+ruzzel "Ruzzel" 
+marc "Marc"
+
+print(lyka)
+print(angel)
+print(ruzzel)
+print(marc)
+
+#manyvalues to multiple x, y, z "Apple", "Banana", "Cherry" print(x, y, z)
+
+#one value to multiple values. x = y = z "Orange" print(x, y, z)
